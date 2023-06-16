631ee99709c144351f20ba14c4aeb58fa4c5b28f
Olia Kravets <94845931+helga20@users.noreply.github.com>
2023-06-16 20:23:59 +0300

    Add files via upload

---
+++ ZNO.CourseWork/ZNO.Front/View/MainMenuWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZNO.Front.View
+{
+    /// <summary>
+    /// Interaction logic for MainMenuWindow.xaml
+    /// </summary>
+    public partial class MainMenuWindow : Window
+    {
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
